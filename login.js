05c4739bf1594d09d3d5b3bab9af0fe1af56f8c1
vishal-tiwari-1971 <160843582+vishal-tiwari-1971@users.noreply.github.com>
2024-02-25 13:43:13 +0530

    Add files via upload
    addtion by use of javascript in login page  it will check that user fill that information correct or not .

---
+++ login.js
@@ -0,0 +1,11 @@
+function log(){
+    var mail=document.getElementById("username").value;
+    var pswd=document.getElementById("password").value;
+     if(mail=="vtiwari15092003@gmail.com" && pswd==123456)
+     {
+        alert(" Login successful");
+     }
+     else{
+        alert(" InvaLid Username or Password")
+     }
+}
